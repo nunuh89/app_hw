e8e6c227a574d0d59e85fd9b994c57110b93ef71
Chi Yang <nunuh89@gmail.com>
2017-10-12 21:39:34 -0700

    impl according to lru principle

--- W2D5/lrucache.rb
+++ W2D5/lrucache.rb
@@ -1,17 +1,30 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @cache = []
+      @size = size
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      # if include; delete first
+      if @cache.include?(el)
+        @cache.delete(el)
+      # if cache is full, remove first element
+      elsif @cache.count == @size
+        @cache.shift
+      end
+      #always add new element to the end
+      @cache << el
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @cache
     end
 
     private
