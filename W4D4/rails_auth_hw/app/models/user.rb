aed1f9661eae211f84083f750f1c5a08bf7a839c
Chi Yang <nunuh89@gmail.com>
2017-10-25 21:38:23 -0700

    add bcypt gem; add routes; add users_controller and new form

--- W4D4/rails_auth_hw/app/models/user.rb
+++ W4D4/rails_auth_hw/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: ture
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
