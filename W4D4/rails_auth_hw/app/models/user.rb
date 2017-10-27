8562d56f3b4347fce97610fdb89454ed1ee8580a
Chi Yang <nunuh89@gmail.com>
2017-10-26 22:11:05 -0700

    add project and replace with auth_token helper

--- W4D4/rails_auth_hw/app/models/user.rb
+++ W4D4/rails_auth_hw/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
+  
 
   def password=(password)
     @password = password
