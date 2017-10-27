cc115ce90b8f0ccc12a8bd00cf9703fa9e921c4a
Chi Yang <nunuh89@gmail.com>
2017-10-26 22:20:32 -0700

    add user_mailer; mail html/txt; add helper methods

--- W4D5/solution/config/environments/development.rb
+++ W4D5/solution/config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # The sent message should pop up in the browser if all went according to plan
+  config.action_mailer.delivery_method = :letter_opener
 end
